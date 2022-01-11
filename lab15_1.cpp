ab3b0371bfa603c4f4a4055c049d34db4a21d373
Nonlnwza007 <waritmanokaew@gmail.com>
2022-01-11 17:01:30 +0700

    finished

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -3,7 +3,20 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
-
+    for (int i = 1; i < N; i++)
+    { 
+        int temp = d[i]; 
+        int n = i - 1;
+		cout <<  "Pass " << i << ':';  
+        while (n >= 0 && d[n] < temp)
+        { 
+            d[n + 1] = d[n]; 
+            n = n - 1;
+        } 
+        d[n + 1] = temp;
+		for(int i = 0; i < 10; i++) cout << d[i] << " ";
+		cout << endl;
+	}
 }
 
 int main(){
--- lab15_2.cpp
+++ lab15_2.cpp
@@ -19,3 +19,37 @@ int main(){
 	showMatrix(B);
 	return 0;
 }
+
+void showMatrix(const bool A[N][N]){
+    for(int j = 0;j < N;j++){
+	for(int i = 0;i < N;i++){
+		cout << A[j][i] << ' ';
+	}
+	cout << endl;
+	}
+	return;
+}
+
+void inputMatrix(double A[N][N]){
+	for(int i = 1;i < 6;i++){
+	cout << "Row " << i << ": ";
+	cin >> A[i-1][0] >> A[i-1][1] >> A[i-1][2] >> A[i-1][3] >> A[i-1][4]; 
+		}
+	return;	
+	}
+
+void findLocalMax(const double A[N][N], bool B[][N]){
+	for(int n = 0;n < 5;n++){
+		B[n][0] = 0;
+		B[0][n] = 0;
+		B[4][n] = 0;
+		B[n][4] = 0;
+	}
+	for(int i = 1;i < 4;i++){
+		for(int j =1;j < 4;j++){
+			if(A[i][j] >= A[i][j-1] && A[i][j] >= A[i][j+1] && A[i][j] >= A[i+1][j] && A[i][j] >= A[i-1][j]) B[i][j] = 1;
+			else B[i][j] = 0;
+		}
+	}
+	return;
+}
